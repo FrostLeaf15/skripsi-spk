1c67d68f6901562cdd4226ff46c2a8c8a7e42281
FrostLeaf15 <85655364+FrostLeaf15@users.noreply.github.com>
2024-08-13 22:51:28 +0700

    this is true last

--- routes/web.php
+++ routes/web.php
@@ -46,147 +46,219 @@ Route::get('/kebutuhandapur', function () {
 });
 
 Route::get('/minuman/buavitaApple', function () {
-    return view('minuman/buavitaApple');
+    return view('minuman/buavitaApple', [
+        "tittle" => "Buavita"
+    ]);
 });
 
 Route::get('/minuman/nescafe', function () {
-    return view('minuman/nescafe');
+    return view('minuman/nescafe', [
+        "tittle" => "Nescafe"
+    ]);
 });
 
 Route::get('/minuman/freshTea', function () {
-    return view('minuman/freshTea');
+    return view('minuman/freshTea', [
+        "tittle" => "Fresh Tea"
+    ]);
 });
 
 Route::get('/minuman/abcKopiSusu', function () {
-    return view('minuman/abcKopiSusu');
+    return view('minuman/abcKopiSusu', [
+        "tittle" => "ABC Kopi Susu"
+    ]);
 });
 
 Route::get('/minuman/sprite', function () {
-    return view('minuman/sprite');
+    return view('minuman/sprite', [
+        "tittle" => "Sprite"
+    ]);
 });
 
 Route::get('/minuman/youC1000', function () {
-    return view('minuman/youC1000');
+    return view('minuman/youC1000', [
+        "tittle" => "You C1000"
+    ]);
 });
 
 Route::get('/kebutuhandapur/nice', function () {
-    return view('kebutuhandapur/nice');
+    return view('kebutuhandapur/nice', [
+        "tittle" => "Nice"
+    ]);
 });
 
 Route::get('/kebutuhandapur/rinso', function () {
-    return view('kebutuhandapur/rinso');
+    return view('kebutuhandapur/rinso', [
+        "tittle" => "Rinso"
+    ]);
 });
 
 Route::get('/kebutuhandapur/vape', function () {
-    return view('kebutuhandapur/vape');
+    return view('kebutuhandapur/vape', [
+        "tittle" => "One Push Vape"
+    ]);
 });
 
 Route::get('/kebutuhandapur/stella', function () {
-    return view('kebutuhandapur/stella');
+    return view('kebutuhandapur/stella', [
+        "tittle" => "Stella"
+    ]);
 });
 
 Route::get('/kebutuhandapur/sunlight', function () {
-    return view('kebutuhandapur/sunlight');
+    return view('kebutuhandapur/sunlight', [
+        "tittle" => "Sunlight"
+    ]);
 });
 
 Route::get('/kebutuhandapur/soklin', function () {
-    return view('kebutuhandapur/soklin');
+    return view('kebutuhandapur/soklin', [
+        "tittle" => "So Klin"
+    ]);
 });
 
 Route::get('/kesehatan/antangin', function () {
-    return view('kesehatan/antangin');
+    return view('kesehatan/antangin', [
+        "tittle" => "Antangin"
+    ]);
 });
 
 Route::get('/kesehatan/bodrex', function () {
-    return view('kesehatan/bodrex');
+    return view('kesehatan/bodrex', [
+        "tittle" => "Bodrex"
+    ]);
 });
 
 Route::get('/kesehatan/hotincream', function () {
-    return view('kesehatan/hotincream');
+    return view('kesehatan/hotincream', [
+        "tittle" => "Hot In Cream"
+    ]);
 });
 
 Route::get('/kesehatan/neurobion', function () {
-    return view('kesehatan/neurobion');
+    return view('kesehatan/neurobion', [
+        "tittle" => "Neurobion"
+    ]);
 });
 
 Route::get('/kesehatan/salonpas', function () {
-    return view('kesehatan/salonpas');
+    return view('kesehatan/salonpas', [
+        "tittle" => "Salonpas"
+    ]);
 });
 
 Route::get('/kesehatan/sangobion', function () {
-    return view('kesehatan/sangobion');
+    return view('kesehatan/sangobion', [
+        "tittle" => "Sangobion"
+    ]);
 });
 
 Route::get('/makanan/qtela', function () {
-    return view('makanan/qtela');
+    return view('makanan/qtela', [
+        "tittle" => "Qtela"
+    ]);
 });
 
 Route::get('/makanan/kopiko', function () {
-    return view('makanan/kopiko');
+    return view('makanan/kopiko', [
+        "tittle" => "Kopiko"
+    ]);
 });
 
 Route::get('/makanan/happydent', function () {
-    return view('makanan/happydent');
+    return view('makanan/happydent', [
+        "tittle" => "Happydent"
+    ]);
 });
 
 Route::get('/makanan/rebo', function () {
-    return view('makanan/rebo');
+    return view('makanan/rebo', [
+        "tittle" => "Kuaci Rebo"
+    ]);
 });
 
 Route::get('/makanan/dorayaki', function () {
-    return view('makanan/dorayaki');
+    return view('makanan/dorayaki', [
+        "tittle" => "Sari Roti Dorayaki"
+    ]);
 });
 
 Route::get('/makanan/rotitawar', function () {
-    return view('makanan/rotitawar');
+    return view('makanan/rotitawar', [
+        "tittle" => "Sari Roti Roti Tawar"
+    ]);
 });
 
 Route::get('/makananHewan/meo', function () {
-    return view('makananHewan/meo');
+    return view('makananHewan/meo', [
+        "tittle" => "Me-O"
+    ]);
 });
 
 Route::get('/makananHewan/pedigree', function () {
-    return view('makananHewan/pedigree');
+    return view('makananHewan/pedigree', [
+        "tittle" => "Pedigree"
+    ]);
 });
 
 Route::get('/makananHewan/smartheartcreamy', function () {
-    return view('makananHewan/smartheartcreamy');
+    return view('makananHewan/smartheartcreamy', [
+        "tittle" => "SmartHeart"
+    ]);
 });
 
 Route::get('/makananHewan/smartheartadult', function () {
-    return view('makananHewan/smartheartadult');
+    return view('makananHewan/smartheartadult', [
+        "tittle" => "SmartHeart"
+    ]);
 });
 
 Route::get('/makananHewan/whiskas', function () {
-    return view('makananHewan/whiskas');
+    return view('makananHewan/whiskas', [
+        "tittle" => "Whiskas"
+    ]);
 });
 
 Route::get('/makananHewan/smartheartdog', function () {
-    return view('makananHewan/smartheartdog');
+    return view('makananHewan/smartheartdog', [
+        "tittle" => "SmartHeart"
+    ]);
 });
 
 Route::get('/personalcare/casablanca', function () {
-    return view('personalcare/casablanca');
+    return view('personalcare/casablanca', [
+        "tittle" => "Casablanca"
+    ]);
 });
 
 Route::get('/personalcare/laurier', function () {
-    return view('personalcare/laurier');
+    return view('personalcare/laurier', [
+        "tittle" => "Laurier"
+    ]);
 });
 
 Route::get('/personalcare/nivea', function () {
-    return view('personalcare/nivea');
+    return view('personalcare/nivea', [
+        "tittle" => "Nivea"
+    ]);
 });
 
 Route::get('/personalcare/pantene', function () {
-    return view('personalcare/pantene');
+    return view('personalcare/pantene', [
+        "tittle" => "Pantene"
+    ]);
 });
 
 Route::get('/personalcare/lifeboy', function () {
-    return view('personalcare/lifeboy');
+    return view('personalcare/lifeboy', [
+        "tittle" => "Lifeboy"
+    ]);
 });
 
 Route::get('/personalcare/emina', function () {
-    return view('personalcare/emina');
+    return view('personalcare/emina', [
+        "tittle" => "Emina"
+    ]);
 });
 
 Route::get('/input', function () {
